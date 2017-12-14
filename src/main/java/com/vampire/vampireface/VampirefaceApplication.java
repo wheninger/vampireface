8cc4f4bb31260c8eb87645e284ee07e617234c61
lei.wang25 <lei.wang25@ucarinc.com>
2017-12-14 17:04:44 +0800

    springboot start

---
+++ src/main/java/com/vampire/vampireface/VampirefaceApplication.java
@@ -0,0 +1,12 @@
+package com.vampire.vampireface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VampirefaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VampirefaceApplication.class, args);
+	}
+}
